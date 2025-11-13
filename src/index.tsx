a42aab8e7827f573e334d2210f52577591fd361e
adityaarakal <adityararakal@gmail.com>
2025-11-13 08:31:29 +0530

    Fix service worker port conflict in development mode

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,12 @@ root.render(
 );
 
 // Register service worker for PWA
-serviceWorkerRegistration.register();
+// Unregister in development to avoid port conflicts
+if (process.env.NODE_ENV === 'production') {
+  serviceWorkerRegistration.register();
+} else {
+  serviceWorkerRegistration.unregister();
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
